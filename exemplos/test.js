d1ce247b3e04395e01025cd486a0c22ba7d2c730
Jesiel Viana <jesielviana@proton.me>
2023-03-27 14:28:21 -0300

    add link do app vereda

--- exemplos/test.js
+++ exemplos/test.js
@@ -1,10 +1,18 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-//1035
-let valores = lines.shift()
-let [a,b,c,d] = valores.split(' ')
-if(b > c && d > a && (c+d) > (a+b) && c > 0 && d > 0 && a % 2 == 0){
-    console.log('Valores aceitos')
-}else{
-    console.log('Valores nao aceitos')
+let entrada = '7.0 5.0 7.0';
+let [a, b, c] = entrada.split(" ").sort((x, y) => y - x);
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+if (a >= b + c) {
+  console.log("NAO FORMA TRIANGULO");
+} else if (a ** 2 == b ** 2 + c ** 2) {
+  console.log("TRIANGULO RETANGULO");
+} else if (a ** 2 > b ** 2 + c ** 2) {
+  console.log("TRIANGULO OBTUSANGULO");
+} else if (a ** 2 < b ** 2 + c ** 2) {
+  console.log("TRIANGULO ACUTANGULO");
+} else if (a == b && b == c) {
+  console.log("TRIANGULO EQUILATERO");
+} else if (a == b || b == c || a == c) {
+  console.log("TRIANGULO ISOSCELES");
 }
