34c4d2e818d64a104d1aee1affc2b2ae2570502d
Jesiel Viana <jesielviana@proton.me>
2023-04-03 15:31:53 -0300

    add slides

--- exemplos/test.js
+++ exemplos/test.js
@@ -1,3 +1,9 @@
-for (let i = 101; i <= 500; i=i+2) {
-  console.log(i);
+// Escreva um programa que imprima o somatório de 0 até 10.
+
+let somatorio = 0;
+
+for (let i = 1; i <= 10; i++) {
+  somatorio = somatorio + i;
 }
+
+console.log(somatorio);
